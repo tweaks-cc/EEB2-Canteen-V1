50bfea79f704709d8009e36acaba2c90aa3fcd71
JoFaHD <73747175+JoFaHD@users.noreply.github.com>
2022-10-13 21:24:33 +0200

    pdfreader geht
    liest Zeile für zeile aka pain
    muss aber passen

--- pdfreader.py
+++ pdfreader.py
@@ -1,5 +1,5 @@
 import pdfplumber
 
-with pdfplumber.open("document_path.PDF") as temp:
+with pdfplumber.open("menues/menu.pdf") as temp:
   first_page = temp.pages[0]
   print(first_page.extract_text())
