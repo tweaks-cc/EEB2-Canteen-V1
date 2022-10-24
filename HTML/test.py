f670a21a87e9cde671b96a9fd880f4116ec1fd64
JoFaHD <73747175+JoFaHD@users.noreply.github.com>
2022-10-24 20:15:04 +0200

    geht auch im js

--- HTML/test.py
+++ HTML/test.py
@@ -1,6 +1,6 @@
 with open("main.html", "r") as html:
     htmlcontent = html.read()
 
-htmlcontent = htmlcontent.replace("<!--to replace-->","<!--Hello from Python-->")
+htmlcontent = htmlcontent.replace('"to replace"','"<!--Hello from Python-->"')
 with open("main.html", "w") as html:
     html.write(htmlcontent)
