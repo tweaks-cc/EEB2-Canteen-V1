8b182752ae748b17ba18455f97156c2426972c2e
JoFaHD <73747175+JoFaHD@users.noreply.github.com>
2022-10-27 20:45:30 +0200

    archiv erstellung

---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>Dieser branch ist nur für archivierte Dateien</h1>
+Bitte nicht mergen
+<br>
+<br>ist an und für sich nur da, um die Statistik der verwendeten sprachen zu fixen
--- webseite/test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-with open("output.js", "w") as outputfile:
-    with open("..//Auswerten-Python//octobre.json", "r") as outputjson:
-        outputfile.write("const essensdict = " + outputjson.read())
