3c0fef335ee7f9ec15ad01ac75df3c6388a64473
Hypnoid <116501325+Hypnoid@users.noreply.github.com>
2023-02-05 19:51:21 +0100

    Rewrite is now operational
    The rewritten script is now operational, but there is no documentation and debugging. Commit coming soon

---
+++ shell-scripts/fetchXlsx.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+br="printf "\n""
+dir=../auswerten-python/menues
+weekNum="$(printf $((
+$(date +%-W) - $(date +%-W -d "$(date +%Y)/$(date +%m)/01")
+)))"
+
+fetch() {
+	curl -f https://www.woluweparents.org/wp-content/uploads/$(date +%Y -d "-1 Month")/$(date +%m -d "-1 Month")/Semaine-$(date +%W -d "$1 Weeks")-$(date +%d -d "Sunday -6 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -6 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y)-$(date +%d -d "Sunday -2 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -2 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y).xlsx -o $dir/input-$(date +%W -d "$1 Weeks")-$(date +%Y -d "$1 Weeks").xlsx \
+	|| curl -f https://www.woluweparents.org/wp-content/uploads/$(date +%Y)/$(date +%m)/Semaine-$(date +%W -d "$1 Weeks")-$(date +%d -d "Sunday -6 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -6 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y)-$(date +%d -d "Sunday -2 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -2 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y).xlsx -o $dir/input-$(date +%W -d "$1 Weeks")-$(date +%Y -d "$1 Weeks").xlsx
+}
+
+if [[ $weekNum == 0 ]] ; then
+	fetch +0
+	fetch +1
+	fetch +2
+	fetch +3
+	fetch +4
+elif [[ $weekNum == 1 ]] ; then
+	fetch -1
+	fetch +0
+	fetch +1
+	fetch +2
+	fetch +3
+elif [[ $weekNum == 2 ]] ; then
+	fetch -2
+	fetch -1
+	fetch +0
+	fetch +1
+	fetch +2
+elif [[ $weekNum == 3 ]] ; then
+	fetch -3
+	fetch -2
+	fetch -1
+	fetch +0
+	fetch +1
+elif [[ $weekNum == 4 ]] ; then
+	fetch -4
+	fetch -3
+	fetch -2
+	fetch -1
+	fetch +0
+else
+	echo "not found"
+fi
