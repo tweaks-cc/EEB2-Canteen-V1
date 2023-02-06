abc42696ed0ae1bbe39795b705b2b601efa74ef9
Hypnoid <116501325+Hypnoid@users.noreply.github.com>
2023-02-06 20:31:36 +0100

    Backup old xlsx files before fetching new ones

--- shell-scripts/fetchXlsx.sh
+++ shell-scripts/fetchXlsx.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 br="printf "\n""
+
+file=input-$(date +%W -d "$1 Weeks")-$(date +%Y -d "$1 Weeks").xlsx
 dir=../auswerten-python/menues
+bakDir=$dir/backup-inputs
 weekNum="$(printf $((
 $(date +%-W) - $(date +%-W -d "$(date +%Y)/$(date +%m)/01")
 )))"
 
 fetch() {
-	curl -f https://www.woluweparents.org/wp-content/uploads/$(date +%Y -d "-1 Month")/$(date +%m -d "-1 Month")/Semaine-$(date +%W -d "$1 Weeks")-$(date +%d -d "Sunday -6 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -6 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y)-$(date +%d -d "Sunday -2 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -2 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y).xlsx -o $dir/input-$(date +%W -d "$1 Weeks")-$(date +%Y -d "$1 Weeks").xlsx \
-	|| curl -f https://www.woluweparents.org/wp-content/uploads/$(date +%Y)/$(date +%m)/Semaine-$(date +%W -d "$1 Weeks")-$(date +%d -d "Sunday -6 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -6 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y)-$(date +%d -d "Sunday -2 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -2 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y).xlsx -o $dir/input-$(date +%W -d "$1 Weeks")-$(date +%Y -d "$1 Weeks").xlsx
+	curl -f https://www.woluweparents.org/wp-content/uploads/$(date +%Y -d "-1 Month")/$(date +%m -d "-1 Month")/Semaine-$(date +%W -d "$1 Weeks")-$(date +%d -d "Sunday -6 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -6 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y)-$(date +%d -d "Sunday -2 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -2 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y).xlsx -o $dir/$file \
+	|| curl -f https://www.woluweparents.org/wp-content/uploads/$(date +%Y)/$(date +%m)/Semaine-$(date +%W -d "$1 Weeks")-$(date +%d -d "Sunday -6 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -6 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y)-$(date +%d -d "Sunday -2 Days $1 Weeks")-$(env LC_TIME=fr_FR.UTF-8 date +%B -d "Sunday -2 Days $1 Weeks" | cut -c 1-4 | iconv -t ASCII//TRANSLIT)-$(date +%Y).xlsx -o $dir/$file
 }
+
+mv -f $dir/input-*.xlsx $bakDir
 
 if [[ $weekNum == 0 ]] ; then
 	fetch +0
