f4dd061216a9ef39910d480083e5e85e587ed759
JoFaHD <73747175+JoFaHD@users.noreply.github.com>
2023-03-03 13:59:03 +0100

    Further tests for exec, here with filereading

--- auswerten-python/test & TODO/test exec().py
+++ auswerten-python/test & TODO/test exec().py
@@ -5,5 +5,12 @@ exec(vardec)
 exec(varchange)
 print(myvar)
 
+with open("toread.txt") as file:
+    readexec = file.read()
+
+exec(readexec)
+print(readvar)
+print(readvar[1])
+
 # Kann benutzt werden um das alte JS Menü als var einzulesen und zu benutzen
 # Muss so, da länge ja dynamisch, mit Index geht also nicht
